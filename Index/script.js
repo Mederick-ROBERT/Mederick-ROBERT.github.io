88b1032a2990e6dc76d87d7e24113fea5adb1ca6
Mederick-ROBERT <mederick.robert@free.fr>
2023-05-04 16:55:26 +0200

    Ajout du portFolio

---
+++ Index/script.js
@@ -0,0 +1,16 @@
+ 
+  document.getElementById("scrollhome").addEventListener("click", function() {
+    document.getElementById("home").scrollIntoView({behavior: "smooth"});
+  });
+
+  document.getElementById("scrollabout").addEventListener("click", function() {
+    document.getElementById("about").scrollIntoView({behavior: "smooth"});
+  });
+
+  document.getElementById("scrollproject").addEventListener("click", function() {
+    document.getElementById("project").scrollIntoView({behavior: "smooth"});
+  });
+
+  document.getElementById("scrollcontact").addEventListener("click", function() {
+    document.getElementById("contact").scrollIntoView({behavior: "smooth"});
+  });
